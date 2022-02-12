1da8d3abe5cefc788610e355d1e5d87336a65622
= <richardoehbet221@gmail.com>
2022-02-12 13:15:50 +0700

    update menubar

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,5 +1,44 @@
+import logo from "../../assets/images/logo.svg";
+import {
+  AiOutlineShoppingCart,
+  AiOutlineOrderedList,
+  AiOutlineHistory,
+} from "react-icons/ai";
+import { GoGraph } from "react-icons/go";
+
 function MenuBar() {
-  return <div>MenuBar</div>;
+  return (
+    <div className="min-h-screen bg-white w-[8%] flex flex-col items-center justify-between py-8">
+      <div className="">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="">
+        <ul className="flex flex-col">
+          <li className="">
+            <a href="/">
+              <AiOutlineOrderedList fontSize={24} />
+            </a>
+          </li>
+          <li className="mt-14">
+            <a href="/">
+              <AiOutlineHistory fontSize={24} />
+            </a>
+          </li>
+          <li className="mt-14">
+            <a href="/">
+              <GoGraph fontSize={24} />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="w-14 h-14 bg-yellow-600 p-2 rounded-full flex items-center justify-center relative text-white">
+        <AiOutlineShoppingCart fontSize={24} />
+        <div className="px-2 bg-red-500 absolute rounded-md -top-1 -right-1">
+          3
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MenuBar;

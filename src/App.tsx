e3d00e8824c4963f16c4d557303750cd84e71bba
= <richardoehbet221@gmail.com>
2022-02-12 12:45:48 +0700

    layouting view

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import ActionBar from "./components/ActionBar";
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App text-red-400">
-      First
+    <div className="App">
+      <MenuBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <ActionBar />
     </div>
   );
 }
---
+++ src/components/ActionBar/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ActionBar() {
+  return <div>ActionBar</div>;
+}
+
+export default ActionBar;

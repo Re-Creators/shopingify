7c4e17f4c575eef242d2027f357337f81bd84ac4
= <richardoehbet221@gmail.com>
2022-03-01 11:27:54 +0700

    fix : category with lengt 0

--- src/components/items/CategoryItems.tsx
+++ src/components/items/CategoryItems.tsx
@@ -5,23 +5,25 @@ function CategoryItems() {
   const categories = useAppSelector((state) => state.categoryItem.categoryItem);
   return (
     <div>
-      {categories.map((category) => (
-        <div className="mt-5" key={category._id}>
-          <h2 className="font-semibold">{category.name}</h2>
-          <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 mt-3">
-            {category.items.map((item) => (
-              <CategoryItem
-                key={item._id}
-                item={item}
-                category={{
-                  _id: category._id,
-                  name: category.name,
-                }}
-              />
-            ))}
+      {categories
+        .filter((category) => category.items.length > 0)
+        .map((category) => (
+          <div className="mt-5" key={category._id}>
+            <h2 className="font-semibold">{category.name}</h2>
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 mt-3">
+              {category.items.map((item) => (
+                <CategoryItem
+                  key={item._id}
+                  item={item}
+                  category={{
+                    _id: category._id,
+                    name: category.name,
+                  }}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
